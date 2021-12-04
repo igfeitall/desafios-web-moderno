45fe82d414d6fa654a50debaf6b0b2ac6658026e
igor feital <igfeitall@gmail.com>
2021-12-04 13:17:39 -0300

    update index.js

--- Node/funcionarios/index.js
+++ Node/funcionarios/index.js
@@ -1,4 +1,8 @@
-const axios = require('axios')
+const axios = require('axios');
 
-const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 
+axios.get(url).then(res => {
+  const funcionarios = res.data;
+  console.log(funcionarios);
+})
