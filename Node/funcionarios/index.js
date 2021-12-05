f522b12f54f95917df3501b192bbc574258aec29
igor feital <igfeitall@gmail.com>
2021-12-05 16:34:38 -0300

    inclusão funcionários

--- Node/funcionarios/index.js
+++ Node/funcionarios/index.js
@@ -1,8 +1,21 @@
+// descobrindo a mulher chinesa com o menor salário
 const axios = require('axios');
 
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 
+//funções para mapeamento do objeto
+const acharMulher = f => f.genero === 'M';
+const acharChines = f => f.pais === 'China';
+const menorSalario = (acc,cur) => {
+   if(cur.salario <= acc.salario) return cur
+   else return acc
+} 
+
 axios.get(url).then(res => {
   const funcionarios = res.data;
-  console.log(funcionarios);
+
+  const mulheresChinesas = funcionarios.filter(acharMulher).filter(acharChines);
+  const resultado = mulheresChinesas.reduce(menorSalario)
+
+  console.log(resultado);
 })
