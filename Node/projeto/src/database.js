68b246720964237c3d2c0b581d9cb6c0f5c05cda
igor feital <igfeitall@gmail.com>
2021-12-06 15:48:31 -0300

    inclusao do banco em memória

---
+++ Node/projeto/src/database.js
@@ -0,0 +1,23 @@
+const sequence = {
+  _id = 1,
+  id = () => this._id++
+}
+
+const bd = {};
+
+function createProduct(product){
+  if(!product.id) product.id = sequence.id();
+  bd[product.id] = product;
+
+  return product;
+}
+
+function getProducts(){
+  return Object.values(bd);
+}
+
+function getProduct(id){
+  return bd[id] || {};
+}
+
+module.exports = {createProduct, getProduct, getProducts}
