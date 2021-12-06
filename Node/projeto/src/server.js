c7be05d9bcfaca9b8c9a90d2d53630b1ac2ca747
igor feital <igfeitall@gmail.com>
2021-12-06 16:31:36 -0300

    incluindo CRUD

--- Node/projeto/src/server.js
+++ Node/projeto/src/server.js
@@ -1,21 +1,41 @@
-//criar uma api rest com banco em memoria para manejar produtos
+//criar uma api rest com banco em memoria com CRUD de produtos
 const express = require('express');
 const bd = require('./database');
 
 const port = 3003;
 const app = express();
 
-app.get('/products',(req,res) => {
-  res.send(bd.getProducts)
-})
+app.use(express.json());
 
-app.get('/produtct/:id',(req,res) => {
-  res.send(bd.getProduct(req.params.id))
-})
-
+//CREATE
 app.post('/product', (req,res) => {
   const {nome, preco} = req.body;
   const produto = bd.createProduct({nome,preco})
+
+  res.send(produto)
+})
+
+//READ
+app.get('/product',(req,res) => {
+  res.send(bd.getProducts())
+})
+
+app.get('/product/:id',(req,res) => {
+  res.send(bd.getProduct(req.params.id))
+})
+
+//UPDATE
+app.put('/product/:id', (req,res) => {
+  const id = Number.parseInt(req.params.id);
+  const {nome, preco} = req.body;
+  const produto = bd.createProduct({id, nome, preco})
+
+  res.send(produto)
+})
+
+//DELETE
+app.delete('/product/:id', (req,res) => {
+  const produto = bd.deleteProduct(req.params.id);
 
   res.send(produto)
 })
